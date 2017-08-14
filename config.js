102c05a7d328ebd3e276bdec2b55f8fe1acd621f
thaopv <pvthaoit@gmail.com>
2017-08-14 23:35:00 +0700

    connect mongo

--- config.js
+++ config.js
@@ -2,6 +2,9 @@ module.exports = {
 	dev: {
 		server: {
 			port: 9000
+		},
+		db: {
+			address: 'mongodb://127.0.0.1/booking-library'
 		}
 	}
 };
--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = module.exports = express();
 const env = process.env.NODE_ENV || 'dev';
 const config = require('./config')[env];
 
+// connect mongodb with mongoose
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect(config.db.address);
+
 // start server
 var server = app.listen(config.server.port, function() {
 	var host = server.address().address;
