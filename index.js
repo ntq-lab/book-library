572c22e6d403c8d02679c811417b64164bbbffbc
NgocTV <chipnin@gmail.com>
2017-08-16 09:04:07 +0700

    feature/#9 - Add base code about Pug view engine

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = module.exports = express();
 const env = process.env.NODE_ENV || 'dev';
 const config = require('./config')[env];
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.use(require('./controllers'));
+
 // start server
 var server = app.listen(config.server.port, function() {
 	var host = server.address().address;
